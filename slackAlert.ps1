2fc4e5be87a3e93b04c3b96e663f7fd2a744363f
David Burkitt <davidburkitt@hotmail.com>
2020-06-23 15:05:01 +0100

    added slack alerting

---
+++ slackAlert.ps1
@@ -0,0 +1,29 @@
+param (
+   # [String] $ApplicationId,
+   # [String] $ClientSecret,
+   # [String] $TenantId,
+   [String] $SlackNotificationUrl
+   [String] $SlackNotification
+)
+
+function Send-SlackMessage([string]$Message) {
+    Invoke-WebRequest -Uri $SlackNotificationUrl -Method Post -Headers @{"content-type"="application/json"} -Body $Message
+}
+
+$payload = [System.Text.StringBuilder]::new(@"
+{
+	"blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "$($SlackNotification)"
+            }
+        },
+        {
+            "type": "divider"
+        }
+"@)
+$payload.Append("]}")
+
+Send-SlackMessage -Message $payload.ToString()
