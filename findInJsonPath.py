31638c32c7e2fcca42664731a0e9326b119ad408
David Burkitt <davidburkitt@hotmail.com>
2020-06-24 10:42:19 +0100

    jsonPath work around

--- findInJsonPath.py
+++ findInJsonPath.py
@@ -1,15 +1,21 @@
 import json, sys
-from jsonpath_ng import jsonpath, parse
+from jsonpath_ng import parse
 
 with open(sys.argv[1]) as json_file:
   json_data = json.load(json_file)
 
-#jsonpath_expression = parse('last_run.summary.*.flagged_items')
 jsonpath_expression = parse(sys.argv[2])
-for match in jsonpath_expression.find(json_data):
-  if isinstance(match.value,int):
-    if match.value > 0:
-      print(str(match.full_path)+":"+str(match.value))
-  if isinstance(match.value,dict):
-    print(json.dumps(match.value, indent=4, sort_keys=True))
 
+skip = False
+for match in jsonpath_expression.find(json_data):
+  # Work around - unable to filter 'findings' e.g. services.*.findings.*[?(@.flagged_items > 0)].items,description
+  # Because filter mechanism only applies to list object and findings are dict
+  if isinstance(match.value,int):
+    if match.value == 0:
+      skip = True
+      continue
+    else:
+      skip = False
+      continue
+  elif not skip:
+    print(json.dumps(match.value, indent=4, sort_keys=True))
